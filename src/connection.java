9e6664b884b804c7252aa1d9b708d786b9a2e95d
Florent <yiminflorent@gmail.com>
2021-07-21 16:15:49 -0700

    MySQL CONNECTION

---
+++ src/connection.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class connection {
+    static final String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:database-bank.cftlrisfj7mu.us-west-2.rds.amazonaws.com:3306/BankDB";
+    static final String USER = "admin";
+    static final String PASS = "password";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName(JDBC_Driver);
+
+            System.out.println("Connecting to your database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+            String sql = "CREATE TABLE TestJava";
+            stmt.executeUpdate(sql);
+            System.out.println("Created table in your database!");
+            }
+        catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            }
+    }
+}
