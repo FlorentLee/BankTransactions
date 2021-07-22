7bc299bceb00535b39abf4cd12a2c90424d11baf
Florent <yiminflorent@gmail.com>
2021-07-21 17:35:42 -0700

    MySQL CONNECTION

---
+++ src/com/yimin/bank/MySQL_DAO.java
@@ -0,0 +1,53 @@
+package com.yimin.bank;
+
+public class MySQL_DAO implements InterfaceDAO {
+    @Override
+    public void addUser() {
+
+    }
+
+    @Override
+    public void updateName(Object o, String newName) {
+
+    }
+
+    @Override
+    public void updateEmail(Object o, String newEmail) {
+
+    }
+
+    @Override
+    public void updatePhone(Object o, String newPhone) {
+
+    }
+
+    @Override
+    public void deleteUser(String accountNumber) {
+
+    }
+
+    @Override
+    public void displayAllUsers() {
+
+    }
+
+    @Override
+    public Object getUser(String name) {
+        return null;
+    }
+
+    @Override
+    public void depositMoney(Object o) {
+
+    }
+
+    @Override
+    public void displayBalance(Object o) {
+
+    }
+
+    @Override
+    public void displayDetails(Object o) {
+
+    }
+}
--- src/connection.java
+++ src/connection.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 
 public class connection {
     static final String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:database-bank.cftlrisfj7mu.us-west-2.rds.amazonaws.com:3306/BankDB";
+    static final String DB_URL = "jdbc:mysql://database-bank.cftlrisfj7mu.us-west-2.rds.amazonaws.com:3306/BankDB";
     static final String USER = "admin";
     static final String PASS = "password";
 
@@ -19,7 +19,12 @@ public class connection {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
             stmt = conn.createStatement();
-            String sql = "CREATE TABLE TestJava";
+            String sql = "CREATE TABLE Test " +
+                    "(id INTEGER not NULL, " +
+                    " first VARCHAR(255), " +
+                    " last VARCHAR(255), " +
+                    " age INTEGER, " +
+                    " PRIMARY KEY ( id ))";
             stmt.executeUpdate(sql);
             System.out.println("Created table in your database!");
             }
